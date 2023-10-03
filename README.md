a79babf7b2e88a7ec71765786c1bad3e873a0f75
Dario <dvaldespino00@gmail.com>
2023-10-02 23:54:26 -0400

    feat: initial commit.
    Signed-off-by: Dario <dvaldespino00@gmail.com>

---
+++ README.md
@@ -0,0 +1,4 @@
+# Motion
+
+Native bindings for the Box2D physics engine using Panama. It's called "Motion" because enough Kotlin projects have
+names involving "K".
---
+++ settings.gradle.kts
@@ -0,0 +1,43 @@
+@file: Suppress("UnstableApiUsage", "LocalVariableName")
+
+import org.gradle.api.initialization.resolve.RepositoriesMode.FAIL_ON_PROJECT_REPOS
+import build.less.plugin.settings.buildless
+
+enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
+
+// Basic configuration
+rootProject.name = "motion"
+
+// Project-level plugins
+pluginManagement {
+  repositories {
+    google()
+    mavenCentral()
+    gradlePluginPortal()
+  }
+}
+
+// Settings plugins
+plugins {
+  id("build.less") version("1.0.0-beta1")
+}
+
+// Centralized dependency management
+dependencyResolutionManagement {
+  repositoriesMode.set(FAIL_ON_PROJECT_REPOS)
+
+  repositories {
+    google()
+    mavenCentral()
+    mavenLocal()
+  }
+}
+
+// Remote build cache (Buildless)
+buildless {
+  remoteCache {
+    // allow disabling pushing to the remote cache
+    val cachePush: String? by settings
+    push = cachePush?.toBooleanStrictOrNull() ?: true
+  }
+}
