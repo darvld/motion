f8ae72f73449312fa2bd9809ca6dce1c342b5fad
Dario Valdespino <dvaldespino00@gmail.com>
2023-10-03 20:39:06 -0400

    feat: add publishing infrastructure.
    Signed-off-by: Dario Valdespino <dvaldespino00@gmail.com>

--- packages/core/build.gradle.kts
+++ packages/core/build.gradle.kts
@@ -1,9 +1,14 @@
 plugins {
   alias(libs.plugins.kotlin)
+  alias(libs.plugins.dokkatoo)
 }
 
 kotlin {
   explicitApi()
+}
+
+java {
+  withSourcesJar()
 }
 
 dependencies {
@@ -12,4 +17,19 @@ dependencies {
 
 tasks.test {
   useJUnitPlatform()
+}
+
+tasks.named<Jar>("javadocJar") {
+  from(tasks.named("dokkatooGeneratePublicationHtml"))
+}
+
+publishing.publications.named<MavenPublication>("maven") {
+  from(components["kotlin"])
+  artifact(tasks.named("sourcesJar"))
+  artifact(tasks.named("javadocJar"))
+
+  pom {
+    name = "Motion Core"
+    description = "Low level bindings for the Chipmunk 2D physics engine."
+  }
 }
